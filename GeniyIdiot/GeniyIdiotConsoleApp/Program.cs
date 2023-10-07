3538abfb65faf9ba74097a69125ef187a89179f7
Евгений Богословский <evbgsl@gmail.com>
2023-10-07 12:27:33 +0300

    test sublime

--- GeniyIdiot/GeniyIdiotConsoleApp/Program.cs
+++ GeniyIdiot/GeniyIdiotConsoleApp/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("Как Вас зовут?");
 string UserName = Console.ReadLine();
-// новый комментарий test 123
+// новый комментарий test 123 sublime
 bool flagRepeatTest = true;
 
 while (flagRepeatTest) 
