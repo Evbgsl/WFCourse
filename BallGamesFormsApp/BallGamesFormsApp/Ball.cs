0ce94bdd78ce113a93824a0631072bc0fc802c33
Евгений Богословский <evbgsl@gmail.com>
2024-02-06 19:49:19 +0300

    Start all balls to different sides and speed

--- BallGamesFormsApp/BallGamesFormsApp/Ball.cs
+++ BallGamesFormsApp/BallGamesFormsApp/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,15 @@ namespace BallGamesFormsApp
         private MainForm form;
         protected int x = 150;
         protected int y = 150;
+
+        protected static Random random = new Random();
+
+        private int vx;
+        private int vy;
+
+        private int randomVx;
+        private int randomVy;
+
         protected int ballSize = 70;
 
         public Ball(MainForm form)
@@ -24,6 +34,38 @@ namespace BallGamesFormsApp
             var brush = Brushes.Aqua;
             var rectangle = new Rectangle(x, y, ballSize, ballSize);
             graphics.FillEllipse(brush, rectangle);
+        }
+
+        private void Clear()
+        {
+            var graphics = form.CreateGraphics();
+            var brush = Brushes.White;
+            var rectangle = new Rectangle(x, y, ballSize, ballSize);
+            graphics.FillEllipse(brush, rectangle);
+
+        }
+
+        public void Randomizer()
+        {
+            randomVx = random.Next(2) * 2 - 1;
+            randomVy = random.Next(2) * 2 - 1;
+
+            vx = random.Next(2,20) * randomVx;
+            vy = random.Next(2, 20) * randomVy;
+        }
+
+        private void Go() 
+        {
+            x += vx;
+            y += vy;        
+        }
+
+        public void Move()
+        {
+            Clear();
+            Go();
+            Show();
+        
         }
     }
 
--- BallGamesFormsApp/BallGamesFormsApp/RandomPointBall.cs
+++ BallGamesFormsApp/BallGamesFormsApp/RandomPointBall.cs
@@ -4,7 +4,7 @@ namespace BallGamesFormsApp
 {
     public class RandomPointBall : Ball
     {
-        protected static Random random = new Random();
+        
         public RandomPointBall(MainForm form) : base(form)
         {
             x = random.Next(0, form.ClientSize.Width - 100);
